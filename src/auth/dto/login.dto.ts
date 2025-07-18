44c8852a0c35ae0ee1c884f2edde8b5673e2585f
mcrosciati <mcrosciati@ciandt.com>
2025-07-17 22:36:13 -0300

    Implementado verificações dos campos do login dto com validation pipes

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'Email inválido' })
   email: string;
+
+  @IsNotEmpty({ message: 'Campo de senha não deve estar vazio' })
+  @IsString({ message: 'Senha informada inválida' })
   password: string;
 }
