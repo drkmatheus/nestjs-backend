0e7222d9394c050de8e3d544b99a6a44dd806f66
mcrosciati <mcrosciati@ciandt.com>
2025-07-17 22:00:53 -0300

    Recebendo parametros no controller da request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get(':id')
+  findUserById(@Param('id') id: string) {
+    return `Olá número ${id}`;
+  }
+}
