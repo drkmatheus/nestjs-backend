7365e284c78490e2a35029487efd929542c3fb5b
mcrosciati <mcrosciati@ciandt.com>
2025-07-21 12:15:39 -0300

    Entidade post e dtos implementados

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(
+  PickType(CreatePostDto, ['title', 'coverImgUrl', 'excerpt', 'content']),
+) {
+  @IsOptional()
+  @IsBoolean({ message: 'Campo de publicar deve ser boolean' })
+  published?: boolean;
+}
